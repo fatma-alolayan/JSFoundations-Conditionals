be827781615d69b448efee54e1b2cbaf9b834417
fatma-alolayan <temaalolayan@hotmail.com>
2020-06-15 20:01:20 +0300

    calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 // Define the numbers and the operation here
-
+let num1 = 3;
+let num2 = 5;
+let operator = "y";
 // Write if-statements here
+if (operator === "+") {
+  console.log(num1 + num2);
+} else if (operator === "-") {
+  console.log(num1 - num2);
+} else if (operator === "*") {
+  console.log(num1 * num2);
+} else if (operator === "/") {
+  console.log(num1 / num2);
+} else console.log("error");
